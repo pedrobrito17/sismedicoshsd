8f6d616229d12968eeff4be9a2cd21d1c263700c
Pedro <pedrobrito17@gmail.com>
2018-12-17 16:57:03 -0300

    correcao do codigo para running online3

--- target/classes/static/dragdrop/js/person.js
+++ target/classes/static/dragdrop/js/person.js
@@ -51,24 +51,7 @@ function acionaMenu(event, index, id) {
     //Carrega o menu na página
     $(".menu-med-add").load(endereco + "/escaladragdrop/listamedicos/" + req);
 
-    setTimeout(function () {
-        //Posiciona o menu de acordo com o botão acionado e o tamanho da tela
-        var tamTela = screen.width;
-        var coordX = event.pageX;
-        if (coordX < (tamTela / 2)) {
-            $(".div-medico").css("float", "left");
-            $(".div-funcao").css("float", "left");
-            $(".menu-med-add").css("top", event.pageY - 20);
-            $(".menu-med-add").css("left", coordX - 10);
-            $(".menu-med-add").show();
-        } else {
-            $(".div-medico").css("float", "right");
-            $(".div-funcao").css("float", "right");
-            $(".menu-med-add").css("top", event.pageY - 20);
-            $(".menu-med-add").css("left", coordX - 650);
-            $(".menu-med-add").show();
-        }
-    }, 200);
+    
 
     //gridIndex é uma variável global
     //Recebe o index do grid atual
