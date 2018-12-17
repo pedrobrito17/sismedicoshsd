277417b53bc5c9c98be0c617558e7ddbb4f611c2
Pedro <pedrobrito17@gmail.com>
2018-12-17 16:47:39 -0300

    correcao do codigo para running online

--- target/classes/static/dragdrop/js/person.js
+++ target/classes/static/dragdrop/js/person.js
@@ -48,79 +48,10 @@ function acionaMenu(event, index, id) {
     }
 
     //AJAX
+    //Carrega o menu na página html
     $(".menu-med-add").load(endereco + "/escaladragdrop/listamedicos/" + req);
 
-    //Após o tempo de 100 ms executa o código abaixo
-    setTimeout(function() {
-        //Posiciona o menu de acordo com o botão acionado e o tamanho da tela
-        var tamTela = screen.width;
-        var coordX = event.pageX;
-        if (coordX < (tamTela / 2)) {
-            $(".div-medico").css("float", "left");
-            $(".div-funcao").css("float", "left");
-            $(".menu-med-add").css("top", event.pageY - 20);
-            $(".menu-med-add").css("left", coordX - 10);
-            $(".menu-med-add").show();
-        } else {
-            $(".div-medico").css("float", "right");
-            $(".div-funcao").css("float", "right");
-            $(".menu-med-add").css("top", event.pageY - 20);
-            $(".menu-med-add").css("left", coordX - 650);
-            $(".menu-med-add").show();
-        }
-
-
-        // //Mouse em cima do medico contratado
-        // $("#med-cont").mouseenter(function () {
-        //     $(".add-medico").css("background", "#fff");
-        //     $("#list-med-ass").hide();
-        //     $("#list-med-cont").show();
-        //     $(".div-funcao:visible").hide();
-        //     console.log("Entrou medico contratado"); //Testando
-        // });
-        // //Mostra o div funcao do respectivo medico
-        // $(".med").click(function () {
-        //     $(".div-funcao:visible").hide();
-        //     let idFunc = $(this).attr("id") + "-func";
-        //     $("#" + idFunc).show();
-        // });
-        // //Altera a cor quando em cima da lista de medicos
-        // $(".add-medico").mouseenter(function () {
-        //     $(".add-medico").css("background", "#fff");
-        //     $(this).css("background", "#e0e0e0");
-        // });
-
-        
-        // //Altena os checkbox
-        // $("[type=checkbox]").change(function () {
-        //     let nome = $(this).attr("name");
-        //     if (nome == "m5") {
-        //         $("[name=hro]").prop("checked", false);
-        //     } else {
-        //         $("[name=m5]").prop("checked", false);
-        //     }
-        // });
-        //gridIndex é uma variável global
-        //Recebe o index do grid atual
-        gridIndex = index;
-        
-        // //Mouse em cima do medico associado
-        // $("#med-ass").mouseenter(function() {
-        //     console.log("Entrou medico associado"); //Testando
-        //     $(".add-medico").css("background", "#fff");
-        //     $("#list-med-cont").hide();
-        //     $("#list-med-ass").show();
-        //     $(".div-funcao:visible").hide();
-        //     console.log("Fim do medico associado");
-        // });
-
-
-
-
-    }, 100);
-
-
+    //gridIndex é uma variável global
+    //Recebe o index do grid atual
+    gridIndex = index;
 }
-
-
-        
